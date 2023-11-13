bfb3800ae1491aa025ee70c594331b262892beba
Vimakro <131078738+Vimakro@users.noreply.github.com>
2023-11-13 13:01:59 -0300

    Add files via upload

---
+++ SoapConsolaAmori2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoapConsolaAmori2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicioAmori.CountryInfoService servicio = new ServicioAmori.CountryInfoService();
+
+            var informacion = servicio.FullCountryInfoAllCountries();
+
+            Console.WriteLine("Codigo de Continente: " + informacion.FirstOrDefault().sContinentCode);
+            Console.WriteLine("Codigo ISO del Pais: " + informacion.FirstOrDefault().sCurrencyISOCode);
+            Console.WriteLine("Nombre del Pais: " + informacion.FirstOrDefault().sName);
+            Console.WriteLine("Capital del Pais: " + informacion.FirstOrDefault().sCapitalCity);
+            Console.WriteLine("Codigo de telefono: " + informacion.FirstOrDefault().sPhoneCode);
+            Console.WriteLine("Bandera: " + informacion.FirstOrDefault().sCountryFlag);
+            
+            Console.ReadLine();
+        }
+    }
+}
